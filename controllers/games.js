d5aeb9b738f6bd90d1102372d31900c0411d356f
Brigette Morando <bibimorando13@yahoo.com>
2016-05-03 11:46:46 -0700

    players can create and join game from home page. They will assigned to host or to a player of the game

--- controllers/games.js
+++ controllers/games.js
@@ -5,6 +5,7 @@ module.exports = {
   index:            index,
   show:             show,
   create:           create,
+  join:             join,
   addInstruction:   addInstruction,
   destroy:          destroy
 }
@@ -12,7 +13,7 @@ module.exports = {
 function index(req, res, next){
   console.log('index contoller function WORKED')
   Game.find({}, function(err, games){
-    if(err) next(err);
+    if(err) res.json(err);
     res.json(games)
   });
 }
@@ -21,28 +22,49 @@ function show (req, res, next) {
   console.log("show controller")
   var id = req.params.id
   Game.find({_id: id}, function(err, game){
-    if(err) next(err);
+    if(err) res.json(err);
     res.json(game)
   });
 }
 
 function create (req, res, next) {
   console.log("create controller")
-  var newGame = new Game(req.body);
-  console.log(req.body)
+  var newGame = new Game();
   //the curr. user will be host of this game
+  newGame.host_id = req.user._id
+
   newGame.save(function(err, savedGame){
-    if(err) next(err);
+    if(err) res.json(err);
     res.json(savedGame);
   });
 }
+
+function join(req, res, next) {
+  console.log("join function is working")
+  console.log(req.body.gameId)
+  var id = req.body.gameId
+  Game.findById(id, function(err, game){
+    console.log(game)
+    if(err || !game) {
+      res.json(err);
+    } else {
+      game.player_ids = []
+      game.player_ids.push(req.user._id)
+      game.save(function (err, updatedGame) {
+        if(err) res.json(err);
+        res.json(updatedGame)
+      })
+    }
+  });
+}
+
 
 function addInstruction(req, res, next){
   console.log("Adding Instruction controller")
   var id = req.params.id
   Game.findById(id, function(err, game){
     if(err || !game){
-      next(err)
+      res.json(err)
     }else{
       var instruction = {
         task: req.body.task,
@@ -50,7 +72,7 @@ function addInstruction(req, res, next){
       }
       game.instructions.push(instruction)
       game.save(function(err, updatedGame){
-        if(err) next(err)
+        if(err) res.json(err)
         console.log('Added Instruction')
         res.json(updatedGame)
       })
@@ -62,7 +84,7 @@ function destroy(req, res, next){
     var id = req.params.id
   console.log("Deleting game", id)
   Game.remove({_id: id}, function(err, game){
-    if(err) next(err)
+    if(err) res.json(err)
     res.json({message: "Game deleted", _id: id})
   })
 }
--- routes/games.js
+++ routes/games.js
@@ -6,7 +6,9 @@ var gamesController = require("../controllers/games");
 router.get(     '/',       gamesController.index);
 router.get(     '/:id',    gamesController.show);
 router.post(    '/',       gamesController.create);
+router.post(    '/join',   gamesController.join);
 router.put(     '/:id',    gamesController.addInstruction);
 router.delete(  '/:id',    gamesController.destroy);
+
 
 module.exports = router;
