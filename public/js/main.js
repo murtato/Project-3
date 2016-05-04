8aa9407a5a4c262b66525517495c84e4556fb9a3
Victor Tran <murtato@gmail.com>
2016-05-04 15:14:04 -0700

    host can add tasks and they will appear on host game page

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,45 @@
 console.log("Hello World")
+
 $(".button-collapse").sideNav();
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-// Show sideNav
 $('.button-collapse').sideNav('show');
-// Hide sideNav
 $('.button-collapse').sideNav('hide');
+
+// var $gameTask;
+
+// var renderTask = _.template(`
+//   <li id="task-<$= _id $>" class="task-instructions">
+//     <%= task %>
+//   `)
+
+// function getId(jqueryThing) {
+//   return jqueryThing.attr('id')
+// }
+
+// var taskHandler = function (task) {
+//   $("#tasks").append("<li class='task>" + task.instruction)
+// }
+
+
+$(".add-button").on("click", function (e) {
+  e.preventDefault
+  var t = $("#add-input").val()
+  var list = $("#task-list")
+
+  $.ajax({
+    method: "PUT",
+    url: "/api/games/"+ e.target.id,
+    data: {
+      task: t
+    }
+  }).then(function(res){
+    list.empty();
+    res.instructions.forEach(function(ins, index) {
+      list.append(
+        "<li><div class='collapsible-header'><i class='material-icons'></i>"+
+        "Task " + index +
+        "</div><div class='collapsible-body'><p>" + ins.task + "</p></div></li>"
+        )
+    })
+  })
+})
